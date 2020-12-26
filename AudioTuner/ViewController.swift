234cf3918068a256fcfd4a5992618cecc5316453
eric <ecastronovo@scu.edu>
2020-12-26 14:51:48 -0800

    Initial commit

---
+++ AudioTuner/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  AudioTuner
+//
+//  Created by Eric Castronovo on 12/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("View Will Appear")
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
+    }
+
+    @IBAction func record(_ sender: Any) {
+        print("Record")
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop Recording")
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
+    }
+}
+
